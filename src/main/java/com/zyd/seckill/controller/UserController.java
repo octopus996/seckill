34ab8116667b0411785e974ce70df507a73292b7
Administrator <739861073@qq.com>
2021-11-26 09:04:49 +0800

    1

--- src/main/java/com/zyd/seckill/controller/UserController.java
+++ src/main/java/com/zyd/seckill/controller/UserController.java
@@ -19,6 +19,8 @@ public class UserController {
     public RespBean info(User user){
         return RespBean.success();
     }
+
+
     @RequestMapping("/mq")
     @ResponseBody
     public void mq(){
